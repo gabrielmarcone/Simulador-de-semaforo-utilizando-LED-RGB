594c362870256de8e3a9b6a0102c469fea857e07
Gabriel Marcone <gabrielmarconems@gmail.com>
2025-01-31 12:35:33 -0300

    definindo os pinos do led e criando função para inicializar os componentes gpio

--- Semaforo.c
+++ Semaforo.c
@@ -3,6 +3,24 @@
 #include "hardware/timer.h"
 #include "hardware/clocks.h"
 
+// Definindo os pinos dos LEDs
+#define LED_GREEN 11
+#define LED_BLUE 12
+#define LED_RED 13
+
+// Função para inicializar a GPIO
+void init_gpio() {
+    // Inicializando a GPIO
+    gpio_init(LED_GREEN);
+    gpio_init(LED_BLUE);
+    gpio_init(LED_RED);
+
+    // Configurando os pinos como saída
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+}
+
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     // Put your timeout handler code in here
     return 0;
@@ -10,6 +28,7 @@ int64_t alarm_callback(alarm_id_t id, void *user_data) {
 
 int main() {
     stdio_init_all();
+    init_gpio();
 
     // Timer example code - This example fires off the callback after 2000ms
     add_alarm_in_ms(2000, alarm_callback, NULL, false);
